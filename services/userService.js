fcfd7164daffa847a6cf98d087add15f58a70a4c
charbelfakhry <53810858+charbelfakhry@users.noreply.github.com>
2023-09-28 14:14:05 +0300

    user service

--- services/userService.js
+++ services/userService.js
@@ -1,39 +1,59 @@
-const { query } = require("../database/db")
+const { query } = require("../database/db");
 
 
-const loadUser = async() => {
-    try{
-        let sql = `SELECT * FROM users`
-        const users = await query(sql);
-        return users;
-    }catch(erorr){
-        throw new Error(error);
-    }
+
+const getUsers = async() => {
+    const users = await query(sql);
+    return users;
 }
 
-const loadSingleUser = async(id)=>{
-    try{
-        let sql = `SELECT * FROM users WHERE user_id = ?`;
-        const user = await query(sql, [id]);
-        return user;
-    }catch(erorr){
-        throw new Error(erorr);
-    }
-}
+const getUserById = async(id) =>{
+    let sql = `SELECT * FROM users WHERE user_id = ?`;
+    const user = await query(sql, [id]);
+    return user;
+} 
 
 const insertUser = async(user) =>{
-    const {user_name, user_username, user_password, user_dob} = user;
-    let inserSQL = `INSERT INTO user
-    VALUES 
-    (?, ?, ?, ?)`;
-
-    const result = await query(sql, [user_name, user_username, user_password, moment(user_dob).format("YYYY-MM-DD")]);
-
+    const {user_name, user_username, user_email, user_password, user_dob} = user;
+    let sql = `INSERT INTO user 
+    (user_name, user_username, user_email, user_password, user_dob)
+    VALUES
+    (?, ?, ?, ?, ?);
+    `;
+    const result = await query(sql, 
+        [
+            user_name, 
+            user_username, 
+            user_email, 
+            user_password, 
+            moment(user_dob).format("YYYY-MM-DD")
+        ]);
     return result;
 }
 
+const updateUser = async(user) => {
+    const {user_id, user_name, user_username, user_email, user_password, user_dob} = user;
+
+    let sql = `UPDATE user SET 
+    user_username = ?, 
+    user_name = ?, 
+    user_email = ?,
+    user_password = ?,
+    user_dob = ?
+    WHERE user_id = ?;
+    `;
+
+    const result = await query(sql, [user_username, user_name, user_email, user_password, moment(user_dob).format("YYYY-MM-DD"), user_id]);
+}
+
+const deleteUser = async(id) =>{
+    return await query("DELETE FROM user WHERE user_id = ?", [id]);
+}
+
 module.exports = {
-    loadUser,
-    loadSingleUser,
+    getUsers,
+    getUserById,
     insertUser,
+    updateUser,
+    deleteUser,
 }
